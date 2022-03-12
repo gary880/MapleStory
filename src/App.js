3c912b58ba947fef42037f9f18a47a9e6613095a
gary880 <easycard32145@gmail.com>
2022-03-12 14:56:13 +0800

    fix React router in github page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom";
-import { Routes, Route } from "react-router-dom";
-import { HashRouter as BrowserRouter } from "react-router-dom";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Skills from "./pages/Skills";
@@ -12,7 +11,7 @@ import Skills from "./pages/Skills";
 const App = () =>{
   
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/MapleStory">
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route index element={<Home />} />
