c208a7f1dadbdb662fc5294fc8730b757bd3f7bf
gary880 <easycard32145@gmail.com>
2022-03-12 14:22:46 +0800

    change to hashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { HashRouter as BrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Skills from "./pages/Skills";
